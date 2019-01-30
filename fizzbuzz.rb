0378fa06a60197ddfc52efef45d6298ddb6fae1d
Joan in DC <joanrigdon@gmail.com>
2019-01-30 22:49:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,15 @@
-
+def fizzbuzz(number)
+  if number % 15
+    return "FizzBuzz"
+  elsif number % 3
+    return "Fizz"
+  elsif number % 5 
+    return "Buzz"
+  else
+    return nil
+  end
+end
+  
 
 
 
